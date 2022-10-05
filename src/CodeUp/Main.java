833d086bd9694d9fa523deef4caa1d0f748243c9
S2ujeong <sj980204@gmail.com>
2022-10-05 09:40:46 +0900

    코드업 출력 문제 풀어봤습니다.

---
+++ src/CodeUp/Main.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int Input = sc.nextInt();
+
+        System.out.printf("%d",Input);
+
+    }
+}
