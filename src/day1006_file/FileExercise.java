881d24c1d5e5345b448aeedb81e04ea7f93752a2
S2ujeong <sj980204@gmail.com>
2022-10-06 15:13:38 +0900

    현재 디렉토리 파일 목록보기

---
+++ src/day1006_file/FileExercise.java
@@ -0,0 +1,17 @@
+package day1006_file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileExercise {
+    public static void main(String[] args) throws FileNotFoundException {
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for (File file : files) {
+            System.out.println(file);
+        }
+    }
+
+
+}
