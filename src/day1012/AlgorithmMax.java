5bf4e813d32a1f959894ad6f1571677cda2c1bd2
S2ujeong <sj980204@gmail.com>
2022-10-12 09:39:39 +0900

    알고리즘 공부: Max값 찾기

---
+++ src/day1012/AlgorithmMax.java
@@ -0,0 +1,30 @@
+package day1012;
+
+public class AlgorithmMax {
+
+    public int getMax(int[] arr) {
+        int maxValue = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+
+        }
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 29, 38, 12, 57, 74, 40, 85, 61};
+
+        AlgorithmMax max = new AlgorithmMax();
+        int resault = max.getMax(arr);
+
+        if (resault == 85) {
+            System.out.println("테스트 통과");
+        } else {
+            System.out.println("테스트 실패 입니다. result" + resault);
+        }
+
+    }
+}
