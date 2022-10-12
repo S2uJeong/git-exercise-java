0ac35ba8060f2eadd8f7fd319ba6142f0626df6a
S2ujeong <sj980204@gmail.com>
2022-10-12 10:12:18 +0900

    알고리즘 공부: Max값 찾기 값과 인덱스를 모두 찾을 수 있게 배열을 통해 반환한다.

--- src/day1012/AlgorithmMax.java
+++ src/day1012/AlgorithmMax.java
@@ -2,30 +2,30 @@ package day1012;
 
 public class AlgorithmMax {
 
-    public int getMax(int[] arr) {
-        // int maxValue = 0;
+    public int [] getMax(int[] arr) {
+        int maxIdx = 0;
         int maxValue = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
 
             if (arr[i] > maxValue) {
                 maxValue = arr[i];
+                maxIdx = i;
             }
-
         }
-        return maxValue;
+        return new int [] {maxValue, maxIdx};
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{3, 29, 38, 12, 57, 74, 40, 85, 61};
 
         AlgorithmMax max = new AlgorithmMax();
-        int resault = max.getMax(arr);
+        int [] result = max.getMax(arr);
 
-        if (resault == 85) {
+        if (result[0] == 85 && result[1] == 7) {
             System.out.println("테스트 통과");
         } else {
-            System.out.println("테스트 실패 입니다. result" + resault);
+            System.out.println("테스트 실패 입니다. result: " + result[1]);
         }
 
     }
