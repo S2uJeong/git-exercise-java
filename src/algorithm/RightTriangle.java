688ca9abb56703250788f33368bcfa86d8f8334f
S2ujeong <sj980204@gmail.com>
2022-10-17 09:21:45 +0900

    알고리즘 RightTriangle 직각삼각형

--- src/algorithm/RightTriangle.java
+++ src/algorithm/RightTriangle.java
@@ -2,10 +2,18 @@ package algorithm;
 
 public class RightTriangle {
 
+    public void print(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.printf("\n");
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%c", '*');
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+
+        RightTriangle rt = new RightTriangle();
+
+        rt.print(5);
     }
 }
