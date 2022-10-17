60029a81534df91df0cb86741478817fd93db86c
S2ujeong <sj980204@gmail.com>
2022-10-17 09:58:56 +0900

    알고리즘 RightTriangle 직각삼각형 다형성추가 : 입력하는 문자로 삼각형 출력

--- src/algorithm/RightTriangle.java
+++ src/algorithm/RightTriangle.java
@@ -2,18 +2,27 @@ package algorithm;
 
 public class RightTriangle {
 
+    private String letter = "*";
+
+    public RightTriangle() {}
+
+    public RightTriangle(String letter) {
+       this.letter = letter;
+    }
+
     public void print(int n) {
         for (int i = 0; i < n; i++) {
             System.out.printf("\n");
             for (int j = 0; j <= i; j++) {
-                System.out.printf("%c", '*');
+                System.out.printf("%s", letter);
             }
         }
     }
     public static void main(String[] args) {
 
-        RightTriangle rt = new RightTriangle();
+        RightTriangle rt1 = new RightTriangle("#");
 
-        rt.print(5);
+        rt1.print(5);
+
     }
 }
