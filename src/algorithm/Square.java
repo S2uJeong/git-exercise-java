c9dad5a585ef16d994cc25ad2ddd430a81a76ad6
S2ujeong <sj980204@gmail.com>
2022-10-18 09:19:57 +0900

    손풀기 1. 정사각형 출력 2. 직사각형 출력

---
+++ src/algorithm/Square.java
@@ -0,0 +1,32 @@
+package algorithm;
+
+public class Square {
+
+    public void printSquare(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printRectangle(int x, int y) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Square sq = new Square();
+
+        sq.printSquare(5);
+
+        sq.printRectangle(2,5);
+    }
+}
